4a6a24ca7fd6a7c047c8d8282f01789ed0dbe9c7
ziguai22-collab <ziguai22@gmail.com>
2026-09-02 12:42:41 +0800

    Refine media actions and ask card appearance

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="binggushenghua-pages-v34";
-const ASSETS=["./","./index.html","./styles-v34.css","./app-v34.js","./favicon.svg"];
+const CACHE="binggushenghua-pages-v35";
+const ASSETS=["./","./index.html","./styles-v35.css","./app-v35.js","./favicon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match("./"))))});
